9bc1442a408f6adb9579739e6ecc937ba30ce732
TurquoiseElena <dujevskit3598@masseyhigh.school.nz>
2025-02-17 14:30:08 +1300

    Simple yes / no checker

---
+++ C_01_yes_no_v1.py
@@ -0,0 +1,10 @@
+# Functions to go here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main Routine gose here
+make_statement("Programming is Fun!", "👍")
